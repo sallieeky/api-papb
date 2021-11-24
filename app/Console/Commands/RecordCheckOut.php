280b2adb46558dadad54d019a5c2b7d914ce1aeb
sallieeky <sallieeky@gmail.com>
2021-11-24 14:30:48 +0800

    Fix

--- app/Console/Commands/RecordCheckOut.php
+++ app/Console/Commands/RecordCheckOut.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\CheckOut;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class RecordCheckOut extends Command
@@ -37,6 +40,19 @@ class RecordCheckOut extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $user = User::all();
+        foreach ($user as $us) {
+            $cek = CheckOut::where([['user_id', $us->id], ['tanggal', '=', Carbon::now()->format('Y-m-d')]])->first();
+            if (!$cek) {
+                CheckOut::create([
+                    "user_id" => $us->id,
+                    "keterangan" => "Terlambat",
+                    "jam" => "Terlambat",
+                    "tanggal" => Carbon::now()->format('Y-m-d')
+                ]);
+            }
+        }
+
+        echo "Success record as late";
     }
 }
--- app/Console/Commands/RecordCheckin.php
+++ app/Console/Commands/RecordCheckin.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\CheckIn;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class RecordCheckin extends Command
@@ -37,6 +40,19 @@ class RecordCheckin extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $user = User::all();
+        foreach ($user as $us) {
+            $cek = CheckIn::where([['user_id', $us->id], ['tanggal', '=', Carbon::now()->format('Y-m-d')]])->first();
+            if (!$cek) {
+                CheckIn::create([
+                    "user_id" => $us->id,
+                    "keterangan" => "Terlambat",
+                    "jam" => "Terlambat",
+                    "tanggal" => Carbon::now()->format('Y-m-d')
+                ]);
+            }
+        }
+
+        echo "Success record as late";
     }
 }
