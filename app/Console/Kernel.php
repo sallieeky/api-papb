b853328779cdeb6a6341ecc2af861b2eccd7e4e4
sallieeky <sallieeky@gmail.com>
2021-11-24 14:41:14 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('record:checkin')->dailyAt("08:01");
+        $schedule->command('record:checkout')->dailyAt("16:01");
     }
 
     /**
@@ -34,7 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
